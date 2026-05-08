536acd6cb7f3b58929d91f0473b913e4a47ace1e
Kaiden Riley <kaiden.riley@pleo.io>
2026-05-08 22:46:43 +0200

    chore: wrap web app in ds provider

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
+import { DesignSystemProvider } from "@neiforfaen/design-system"
 import App from "./App"
 
 // biome-ignore lint/style/noNonNullAssertion: Element guaranteed to exist.
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<DesignSystemProvider>
+			<App />
+		</DesignSystemProvider>
 	</StrictMode>,
 )
