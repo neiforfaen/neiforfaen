9652f3457e8b010389ef6b3450a19c22398ebc49
Kaiden Riley <kaiden.riley@pleo.io>
2026-05-09 10:02:36 +0200

    feat: add husky pre-commit script

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,13 +2,14 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "@neiforfaen/ui/globals.css"
-import { App } from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { App } from "./App.tsx"
 
+// biome-ignore lint/style/noNonNullAssertion: Element guaranteed to exist.
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </StrictMode>
+	<StrictMode>
+		<ThemeProvider>
+			<App />
+		</ThemeProvider>
+	</StrictMode>,
 )
