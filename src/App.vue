647d38db522967c70261fb570d6be62a7b949d3f
neiforfaen <kaiden@0x424.kr>
2026-06-06 23:15:48 +0200

    feat: add smooth scroll

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
+import { VueLenis } from "lenis/vue";
+import type { LenisOptions } from "lenis";
 import Footer from "@components/layout/Footer.vue";
 import Header from "@components/layout/Header.vue";
 import Root from "@components/layout/Root.vue";
 import About from "@components/sections/About.vue";
 import Work from "@components/sections/Work.vue";
 import Projects from "./components/sections/Projects.vue";
+
+const lenisOptions: LenisOptions = {
+  autoRaf: true,
+  autoResize: true,
+};
 </script>
 
 <template>
+  <VueLenis root :options="lenisOptions" />
   <Root>
     <Header />
     <About />
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import "./assets/main.css";
+import "lenis/dist/lenis.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
+import LenisVue from "lenis/vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(LenisVue);
+app.mount("#app");
